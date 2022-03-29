dff36b9873d7031f49712bc12b909f75d1a321db
Zheng-Gang Chen <zhenggan@lxplus769.cern.ch>
2022-03-29 06:57:00 +0200

    Shorten Program and Add FakeRate

--- WorkFlow/main.py
+++ WorkFlow/main.py
@@ -4,13 +4,15 @@ import os
 CURRENT_WORKDIR = os.getcwd()
 sys.path.append(CURRENT_WORKDIR)
 import argparse
+import time
+
 parser = argparse.ArgumentParser()
-parser.add_argument('-m','--mode',help='Program Modes',choices=['Init','BuildDir','TrigEff_Calc','TrigEff_Plot','TrigSF_Calc','DrellYanRECO','Fake_Rate'],type=str)
+parser.add_argument('-m','--mode',help='Program Modes',choices=['Init','BuildDir','TrigEff_Calc','TrigEff_Plot','TrigSF_Calc','DrellYanRECO','FakeRate'],type=str)
 
 parser.add_argument('-y','--year',help='year',choices=['2017','2018'],type=str)
 
 parser.add_argument('-c','--channels',help='Channels, ex: DoubleElectron DoubleMuon',nargs='+')
-parser.add_argument('-i','--channel',choices=['DoubleElectron','DoubleMuon','ElectronMuon'],default=None)
+parser.add_argument('-i','--channel',choices=['DoubleElectron','DoubleMuon','ElectronMuon','Electron','Muon'],default=None)
 parser.add_argument('-o','--DirOut',help="Output Directory's Parent,ex: /eos/user/z/zhenggan",type=str)
 parser.add_argument('-t','--task',help="Task",type=str,choices=["TriggerSF","DrellYan","FakeRate"])
 parser.add_argument('-f','--Type',help="MC/Data",type=str,choices=["MC","Data"])
@@ -19,9 +21,12 @@ parser.add_argument('-a','--trigSF_on',action = "store_true",help="This option i
 args = parser.parse_args()
 
 
+
 if args.mode == 'Init':
     if args.year is None:
         raise ValueError('You need to specify ex:[-y/--year 2017]')
+    if args.task is None:
+        raise ValueError('You need to specify ex:[-t/--task TriggerSF]')
     from Utils.General_Tool import MakeDir
 
     RootDIR = './'
@@ -35,7 +40,7 @@ if args.mode == 'Init':
     MakeDir(RootDIR,'configuration')
     MakeDir(RootDIR,'path')
     if args.task == 'TriggerSF':
-        from Trigger_SF.Trigger_Utils import *
+        from Trigger_SF.Init import *
         GenPaths_HLTTrigger_File(args.year)
         GenTrigEffInput_File(args.year) 
         GenLeptonIDSF_File(args.year)
@@ -44,54 +49,61 @@ if args.mode == 'Init':
         GenGoodFlag_File(args.year) 
     
     elif args.task == 'DrellYan':
-        from Drell_Yan.DY_utils import *
+        from Drell_Yan.Init import *
         GenDataPath_File(args.year) ##Need to be checked.
         GenPaths_HLTTriggerCondition_ForAnalyzer_File(args.year)
         GenXsValue_File(args.year) ##Need to be checked.
         GenTriggerSF_Path(args.year)
     elif args.task == 'FakeRate':
-        from Fake_Rate.FR_Utils import *
+        from Fake_Rate.Init import *
         FakeRateConfiguration(args.year)
-        FakeRageFilesIn(args.year)
+        FakeRateFilesIn(args.year)
     else:
         pass
 elif args.mode == 'BuildDir':
     from Utils.Build_Dir import Build_Dir
+    if args.year is None:
+        raise ValueError('Argument: [-y/--year] must be specified.')
+    if args.channels is None:
+        raise ValueError('Argument: [-c/--channels] must be specified.')
+    if args.task is None:
+        raise ValueError('Argument: [-t/--task] must be specified.')
     Build_Dir(args) 
-elif args.mode == 'TrigEff_Calc':
-    from Trigger_SF.Program_Step import * 
-    if args.channel == None:
-        raise ValueError("Channel should be specified or The Specified Channel is Not in the list. ex:[-i/--channel DoubleElectron]")
-    if args.Type == None:
-        raise ValueError("Should Specify the type of input file(s) ex:[-f/--Type MC]")
 
-    Trig_Calc(year = args.year , channel = args.channel, Type = args.Type,nevents=args.nevents)
-elif args.mode == 'TrigEff_Plot':
-    from Trigger_SF.Program_Step import * 
-    if args.channel == 'None':
-        raise ValueError("Channel should be specified or The Specified Channel is Not in the list. ex:[-i/--channel DoubleElectron]")
-    if args.year == 'None':
-        raise ValueError('You need to specify ex:[-y/--year 2017]')
-    
-    Plot_efficiency(channel=args.channel,year=args.year)
-
-
-elif args.mode == 'TrigSF_Calc':
-    if args.channel == 'None':
-        raise ValueError("Channel should be specified or The Specified Channel is Not in the list. ex:[-i/--channel DoubleElectron]")
-    if args.year == 'None':
-        raise ValueError('You need to specify ex:[-y/--year 2017]')
-    from Trigger_SF.Program_Step import * 
-    SF_Calc(channel=args.channel,year=args.year)
-
-elif args.mode =='DrellYanRECO':
-    if args.channel == 'None':
-        raise ValueError("Channel should be specified or The Specified Channel is Not in the list. ex:[-i/--channel DoubleElectron]")
-    if args.year == 'None':
-        raise ValueError('You need to specify ex:[-y/--year 2017]')
-    from Drell_Yan.Program_Step import *
-    Drell_Yan_Reconstruction(year = args.year, channel = args.channel, nevents = args.nevents,trig_SF_on = args.trigSF_on)
 
 else:
-    raise ValueError("Mode should be specified or The Specified Mode is Not in the list.")
+    from Utils.Program_Steps import RuntimeMeasure
+    RuntimeMeasure(args)
+
+#elif args.mode == 'TrigEff_Calc':
+#    from Utils.Program_Steps import * 
+    
+#    RuntimeMeasure(args,Trig_Calc)
+    #Trig_Calc(year = args.year , channel = args.channel, Type = args.Type,nevents=args.nevents)
+
+#elif args.mode == 'TrigEff_Plot':
+#    from Utils.Program_Steps import * 
+#    RuntimeMeasure(args,Plot_efficiency)
+    #Plot_efficiency(channel=args.channel,year=args.year)
+
+
+#elif args.mode == 'TrigSF_Calc':
+#    from Utils.Program_Steps import * 
+#    RuntimeMeasure(args,SF_Calc)
+    #SF_Calc(channel=args.channel,year=args.year)
+
+#elif args.mode =='DrellYanRECO':
+#    from Utils.Program_Steps import *
+#    Drell_Yan_Reconstruction(year = args.year, channel = args.channel, nevents = args.nevents,trig_SF_on = args.trigSF_on)
+
+#elif args.mode == 'FakeRate':
+
+ #   from Utils.Program_Steps import *
+ #   
+ #   FakeRateCalculation(year=args.year,channel=args.channel,nevents=args.nevents,trig_SF_on=args.trigSF_on)
+
+
+
+#else:
+#    raise ValueError("Mode should be specified or The Specified Mode is Not in the list.")
 
